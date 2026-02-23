a4222cad8c6bc5c1df06b8fcccdf5a36acf5546f
paolobellina-lab <paolo.bellina@telmotor.it>
2026-02-23 21:24:45 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+// File sw.js
+const CACHE_NAME = 'infinity-v1';
+
+self.addEventListener('install', (event) => {
+  console.log('SW: Installato');
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Lascia vuoto o gestisci il caching, ma l'evento deve esserci
+});
