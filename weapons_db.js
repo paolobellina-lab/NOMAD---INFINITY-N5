f772253fb3f1f2d3c86eda2d8286be81ebcc93c6
paolobellina-lab <paolo.bellina@telmotor.it>
2026-02-25 14:09:05 +0100

    Create weapons_db.js

---
+++ weapons_db.js
@@ -0,0 +1,8 @@
+window.masterWeaponsDatabase = {
+    "COMBI RIFLE": [ { label: "0-16\"", mod: 3 }, { label: "16-32\"", mod: -3 }, { label: "32-48\"", mod: -6 } ],
+    "MULTI RIFLE": [ { label: "0-16\"", mod: 3 }, { label: "16-32\"", mod: -3 }, { label: "32-48\"", mod: -6 } ],
+    "HMG": [ { label: "0-8\"", mod: -3 }, { label: "8-16\"", mod: 0 }, { label: "16-32\"", mod: 3 }, { label: "32-48\"", mod: -3 } ],
+    "BOARDING SHOTGUN": [ { label: "0-8\"", mod: 3 }, { label: "8-16\"", mod: 0 }, { label: "16-24\"", mod: -3 } ],
+    "MULTI SNIPER RIFLE": [ { label: "0-16\"", mod: -3 }, { label: "16-48\"", mod: 3 }, { label: "48-96\"", mod: -3 } ],
+    "DEFAULT": [ { label: "0-8\"", mod: 3 }, { label: "8-16\"", mod: 0 }, { label: "16-32\"", mod: -3 }, { label: "32-48\"", mod: -6 } ]
+};
