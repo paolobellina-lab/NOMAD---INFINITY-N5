8f5410e25618469146cb6f4df6dd7dcc29634449
paolobellina-lab <paolo.bellina@telmotor.it>
2026-02-25 14:14:44 +0100

    Update weapons_db.js

--- weapons_db.js
+++ weapons_db.js
@@ -1,8 +1,9 @@
 window.masterWeaponsDatabase = {
-    "COMBI RIFLE": [ { label: "0-16\"", mod: 3 }, { label: "16-32\"", mod: -3 }, { label: "32-48\"", mod: -6 } ],
-    "MULTI RIFLE": [ { label: "0-16\"", mod: 3 }, { label: "16-32\"", mod: -3 }, { label: "32-48\"", mod: -6 } ],
-    "HMG": [ { label: "0-8\"", mod: -3 }, { label: "8-16\"", mod: 0 }, { label: "16-32\"", mod: 3 }, { label: "32-48\"", mod: -3 } ],
-    "BOARDING SHOTGUN": [ { label: "0-8\"", mod: 3 }, { label: "8-16\"", mod: 0 }, { label: "16-24\"", mod: -3 } ],
-    "MULTI SNIPER RIFLE": [ { label: "0-16\"", mod: -3 }, { label: "16-48\"", mod: 3 }, { label: "48-96\"", mod: -3 } ],
-    "DEFAULT": [ { label: "0-8\"", mod: 3 }, { label: "8-16\"", mod: 0 }, { label: "16-32\"", mod: -3 }, { label: "32-48\"", mod: -6 } ]
+    "COMBI RIFLE": { burst: 3, dam: "13", ammo: "Normal", attr: "BS", bands: [ { label: "0-16\"", mod: 3 }, { label: "16-32\"", mod: -3 }, { label: "32-48\"", mod: -6 } ] },
+    "MULTI RIFLE": { burst: 3, dam: "13", ammo: "AP / Shock", attr: "BS", bands: [ { label: "0-16\"", mod: 3 }, { label: "16-32\"", mod: -3 }, { label: "32-48\"", mod: -6 } ] },
+    "HMG": { burst: 4, dam: "15", ammo: "Normal", attr: "BS", bands: [ { label: "0-8\"", mod: -3 }, { label: "8-16\"", mod: 0 }, { label: "16-32\"", mod: 3 }, { label: "32-48\"", mod: -3 } ] },
+    "BOARDING SHOTGUN": { burst: 2, dam: "14", ammo: "AP", attr: "BS", bands: [ { label: "0-8\"", mod: 3 }, { label: "8-16\"", mod: 0 }, { label: "16-24\"", mod: -3 } ] },
+    "MULTI SNIPER RIFLE": { burst: 2, dam: "15", ammo: "DA / AP", attr: "BS", bands: [ { label: "0-16\"", mod: -3 }, { label: "16-48\"", mod: 3 }, { label: "48-96\"", mod: -3 } ] },
+    "GRENADES": { burst: 1, dam: "13", ammo: "Normal", attr: "PH", bands: [ { label: "0-8\"", mod: 3 }, { label: "8-16\"", mod: -3 }, { label: "16-24\"", mod: -6 } ] }, // Usa PH invece di BS!
+    "DEFAULT": { burst: 1, dam: "N/A", ammo: "Normal", attr: "BS", bands: [ { label: "0-8\"", mod: 3 }, { label: "8-16\"", mod: 0 }, { label: "16-32\"", mod: -3 } ] }
 };
